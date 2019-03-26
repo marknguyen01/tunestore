8220b7e9d5da604aec7ed4e569087cfa31089232
nnguye59 <nnguyen59@uncc.edu>
2019-03-26 19:24:54 -0400

    Fix Broken Access Control for Giving gifts

--- src/com/tunestore/action/GiveAction.java
+++ src/com/tunestore/action/GiveAction.java
@@ -47,89 +47,95 @@ public class GiveAction extends Action implements IWithDataSource {
     String sessionToken = String.valueOf(request.getSession(true).getAttribute("TOKEN"));
     
     if(token != null && sessionToken != null && token.equals(sessionToken)) {
-        try {
-            conn = dataSource.getConnection();
-            Statement stmt = conn.createStatement();
-            
-            // First, see if we have enough money
-            sql = "SELECT TUNEUSER.BALANCE, "
-              + "CD.PRICE "
-              + "FROM TUNEUSER, CD "
-              + "WHERE TUNEUSER.USERNAME = '"
-              + request.getSession(true).getAttribute("USERNAME")
-              + "' AND CD.ID = "
-              + daf.getString("cd");
-            log.info(sql);
-            ResultSet rs = stmt.executeQuery(sql);
-            boolean canbuy = true;
-            if (rs.next()) {
-              canbuy = rs.getDouble(1) >= rs.getDouble(2);
-            }
-            rs.close();
-            
-            if (! canbuy) {
-              errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("give.nomoney"));
-            } else {
-              // See if the recipient already has it
-              sql = "SELECT COUNT(*) rowcount "
-                + "FROM TUNEUSER_CD "
-                + "WHERE TUNEUSER_CD.TUNEUSER = '"
-                + daf.getString("friend")
-                + "' AND TUNEUSER_CD.CD = "
-                + daf.getString("cd");
-              log.info(sql);
-              rs = stmt.executeQuery(sql);
-              boolean friendhas = false;
-              if (rs.next()) {
-                friendhas = rs.getInt(1) >= 1;
-              }
-              rs.close();
-
-              // Deduct the amount of the CD from my account
-              sql = "UPDATE TUNEUSER SET BALANCE = BALANCE - (SELECT PRICE FROM CD WHERE CD.ID = "
-                + daf.getString("cd")
-                + ") WHERE TUNEUSER.USERNAME = '"
-                + request.getSession(true).getAttribute("USERNAME")
-                + "'";
-              log.info(sql);
-              stmt.executeUpdate(sql);
-
-              if (friendhas) {
-                // Friend has it, so we give them a gift of money
-                sql = "UPDATE TUNEUSER SET BALANCE = BALANCE + (SELECT PRICE FROM CD WHERE CD.ID = "
-                  + daf.getString("cd")
-                  + ") WHERE TUNEUSER.USERNAME = '"
-                  + daf.getString("friend")
-                  + "'";
+    	// check if user is logged in
+    	if(request.getSession(true).getAttribute("USERNAME") != null) {
+            try {
+                conn = dataSource.getConnection();
+                Statement stmt = conn.createStatement();
+                
+                // First, see if we have enough money
+                sql = "SELECT TUNEUSER.BALANCE, "
+                  + "CD.PRICE "
+                  + "FROM TUNEUSER, CD "
+                  + "WHERE TUNEUSER.USERNAME = '"
+                  + request.getSession(true).getAttribute("USERNAME")
+                  + "' AND CD.ID = "
+                  + daf.getString("cd");
                 log.info(sql);
-                stmt.executeUpdate(sql);
-                messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("give.already"));
-              } else {
-                sql = "INSERT INTO TUNEUSER_CD (TUNEUSER, CD) VALUES ('"
-                  + daf.getString("friend")
-                  + "', "
-                  + daf.getString("cd")
-                  + ")";
-                log.info(sql);
-                stmt.executeUpdate(sql);
-                messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("give.success"));
+                ResultSet rs = stmt.executeQuery(sql);
+                boolean canbuy = true;
+                if (rs.next()) {
+                  canbuy = rs.getDouble(1) >= rs.getDouble(2);
+                }
+                rs.close();
+                
+                if (! canbuy) {
+                  errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("give.nomoney"));
+                } else {
+                  // See if the recipient already has it
+                  sql = "SELECT COUNT(*) rowcount "
+                    + "FROM TUNEUSER_CD "
+                    + "WHERE TUNEUSER_CD.TUNEUSER = '"
+                    + daf.getString("friend")
+                    + "' AND TUNEUSER_CD.CD = "
+                    + daf.getString("cd");
+                  log.info(sql);
+                  rs = stmt.executeQuery(sql);
+                  boolean friendhas = false;
+                  if (rs.next()) {
+                    friendhas = rs.getInt(1) >= 1;
+                  }
+                  rs.close();
+
+                  // Deduct the amount of the CD from my account
+                  sql = "UPDATE TUNEUSER SET BALANCE = BALANCE - (SELECT PRICE FROM CD WHERE CD.ID = "
+                    + daf.getString("cd")
+                    + ") WHERE TUNEUSER.USERNAME = '"
+                    + request.getSession(true).getAttribute("USERNAME")
+                    + "'";
+                  log.info(sql);
+                  stmt.executeUpdate(sql);
+
+                  if (friendhas) {
+                    // Friend has it, so we give them a gift of money
+                    sql = "UPDATE TUNEUSER SET BALANCE = BALANCE + (SELECT PRICE FROM CD WHERE CD.ID = "
+                      + daf.getString("cd")
+                      + ") WHERE TUNEUSER.USERNAME = '"
+                      + daf.getString("friend")
+                      + "'";
+                    log.info(sql);
+                    stmt.executeUpdate(sql);
+                    messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("give.already"));
+                  } else {
+                    sql = "INSERT INTO TUNEUSER_CD (TUNEUSER, CD) VALUES ('"
+                      + daf.getString("friend")
+                      + "', "
+                      + daf.getString("cd")
+                      + ")";
+                    log.info(sql);
+                    stmt.executeUpdate(sql);
+                    messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("give.success"));
+                  }
+                }
+              } catch (Exception e) {
+                throw new RuntimeException(e);
+              } finally {
+                if (conn != null) {
+                  try { conn.close(); } catch (Exception e) {}
+                }
               }
-            }
-          } catch (Exception e) {
-            throw new RuntimeException(e);
-          } finally {
-            if (conn != null) {
-              try { conn.close(); } catch (Exception e) {}
-            }
-          }
-        
-        // Assign new token
-        SecureRandom rand = new SecureRandom();
-        request.getSession(true).setAttribute("TOKEN", rand.nextInt(1000));
-        
-          saveErrors(request, errors);
-          saveMessages(request, messages);
-          return retval;
+            
+            // Assign new token
+            SecureRandom rand = new SecureRandom();
+            request.getSession(true).setAttribute("TOKEN", rand.nextInt(1000));
+            
+    	}
+    	else {
+    		messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("Please login in to perform this task!"));
+    	}
+        saveErrors(request, errors);
+        saveMessages(request, messages);
+        return retval;
     }
     else {
     	throw new ServletException("Invalid token!");
